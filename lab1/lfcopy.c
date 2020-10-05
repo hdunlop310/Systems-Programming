d3b883bb4eb5eb4876c876d25fe965f17db7e623
Heather Cook <2375346c@student.gla.ac.uk>
2020-10-05 12:25:35 +0100

    lab1 part2 done

---
+++ lab1/lfcopy.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+
+/* readline: read a line from standard input, return its length or 0
+*/
+int readline(char line[], int max){
+
+    if (fgets(line, max, stdin) == NULL){
+        return 0;
+    }else{
+        return strlen(line);
+    }
+
+}
+/* writeline: write line to standard output, return number of chars
+written */
+int writeline(const char line[]){
+    fputs(line, stdout);
+    return strlen(line);
+}
+
+int main(){
+    int max = 11;
+    char line[max];
+
+    fgets(line, max, stdin);
+    
+    readline(line, max);
+    int no_of_lines = writeline(line);
+    printf("\n%d\n",no_of_lines);
+    
+}
