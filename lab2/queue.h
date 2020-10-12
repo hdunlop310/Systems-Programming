9a8c4a9f79898ffcb85addbb86d16f9798491f3b
Heather Cook <2375346c@student.gla.ac.uk>
2020-10-12 11:17:04 +0100

    added correct lab files

--- lab2/queue.h
+++ lab2/queue.h
@@ -1,5 +1,5 @@
-#ifndef _QUEUE_H_
-#define _QUEUE_H_
+#ifndef _QUEUE_INCLUDED
+#define _QUEUE_INCLUDED
 
 typedef struct queue Queue;
 typedef void *Item;
@@ -8,17 +8,18 @@ typedef void *Item;
 #define MAX_PRIO 1
 
 /*
-* create a Queue that holds Items
-* returns NULL if the create call failed (malloc failure)
-*/
+ * create a Queue that holds Items
+ * returns NULL if the create call failed (malloc failure)
+ */
 Queue *q_create(void);
 /*
-* add Item to the Queue; 3rd arg is priority in MIN_PRIO..MAX_PRIO;
-* return 1/0 if successful/not-successful
-*/
+ * add Item to the Queue; 3rd arg is priority in MIN_PRIO..MAX_PRIO;
+ * return 1/0 if successful/not-successful
+ */
 int q_add(Queue *q, Item i, int prio);
 /*
-* remove next Item from queue; returns NULL if queue is empty
-*/
+ * remove next Item from queue; returns NULL if queue is empty
+ */
 Item q_remove(Queue *q);
+
 #endif /* _QUEUE_INCLUDED */
